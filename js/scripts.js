4a42663d22409f0a2717e6376ef4583d33c72f91
Jordan Mor <jordanmor@gmail.com>
2018-08-15 19:26:19 -0400

    Add Directory and Employee components

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,68 @@ $('#gallery').after(
 );
 
 $('.modal-container').hide();
+
+// $('#gallery').append(
+//   `<div class="card">
+//     <div class="card-img-container">
+//         <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+//     </div>
+//     <div class="card-info-container">
+//         <h3 id="name" class="card-name cap">first last</h3>
+//         <p class="card-text">email</p>
+//         <p class="card-text cap">city, state</p>
+//     </div>
+//   </div>`
+// );
+
+class Directory {
+  constructor() {
+    this.employees = [];
+    this.$gallery = $('#gallery');
+  };
+
+  fetchData(num) {
+    fetch(`https://randomuser.me/api/?nat=us,gb,ca,ie,au&results=${num}&noinfo`)
+        .then(res => res.json())
+        .then(data => data.results)
+        .then(employees => {
+          return employees.map(employee => new Employee(employee));
+        })
+        .then(employeesArray => {
+          this.populateEmployees(employeesArray);
+          this.employees.push(employeesArray);
+        });
+  }
+
+  populateEmployees(employees) {
+    const html = employees.map(employee => {
+      const { name, email, image, city, state} = employee;
+      return `<div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src=${image} alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${name}</h3>
+                    <p class="card-text">${email}</p>
+                    <p class="card-text cap">${city}, ${state}</p>
+                </div>
+              </div>`;
+    });
+    this.$gallery.append(html);
+  }
+
+}
+
+class Employee {
+  constructor(data) {
+    this.name = `${data.name.first} ${data.name.last}`;
+    this.email = data.email;
+    this.image = data.picture.large;
+    this.city = data.location.city;
+    this.state = data.location.state;
+  }
+}
+
+const directory = new Directory();
+directory.fetchData(12);
+console.log(directory.employees)
