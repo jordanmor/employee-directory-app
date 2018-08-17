43539f046694c92a2afe8e90e25413db4c467a5f
Jordan Mor <jordanmor@gmail.com>
2018-08-17 07:00:48 -0400

    Add search input

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,12 @@ $('#gallery').after(
   </div>`
 );
 
+$('.search-container').append(
+  `<form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+  </form>`);
+
 $('.modal-container').hide();
 
 /*=============-=============-=============-=============
